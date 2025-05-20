ecc3ea0b8bd0320a124ce968b1a4ece7bbd105a2
Wyatt James <wyattsamueljames@gmail.com>
2025-05-20 13:37:11 -0600

    feat: add check if playing today

--- pumpjack.sh
+++ pumpjack.sh
@@ -10,22 +10,45 @@ readonly schedule_now_enpt="https://api-web.nhle.com/v1/schedule/${current_date}
 #echo "The current date is ${current_date}"
 
 get_games_today(){
-    schedule_now_data=$(curl -sX GET $schedule_now_enpt)
+    local schedule_now_data=$(curl -sX GET $schedule_now_enpt)
 
-    game_week=$(echo $schedule_now_data | jq -c '.gameWeek[]')
+    local game_week=$(echo $schedule_now_data | jq -c '.gameWeek[]')
     # Not using quotes with the echo flattens the json causing issues...
     echo "$game_week" | while read -r day; do
-        date=$(echo "$day" | jq -r '.date')
+        local date=$(echo "$day" | jq -r '.date')
         if [[ "$date" == "$current_date" ]]; then
-            games=$(echo "$day" | jq -c '.games[]')
+            local games=$(echo "$day" | jq -c '.games[]')
             echo "$games"
         fi
     done
 }
 
+check_if_playing_today(){
+    local games_today="$1"
+    while read -r game; do
+        local game_state=$(echo "$game" | jq -c '.gameState')
 
+        local away_team=$(echo "$game" | jq -c '.awayTeam.abbrev')
+        local home_team=$(echo "$game" | jq -c '.homeTeam.abbrev')
+        if [[ "$home_team" == "$edm_abbrev" || "$away_team" == "$edm_abbrev" ]]; then
+            game_id=$(echo "$game" | jq -c '.id')
+            return 0 # Game is being played today
+        fi
+    return 1 # Game is not being played today
+
+# while read runs in subshell, this triple arrow is a here string that keeps
+# the loop in the same shell so that I can use the return value
+    done <<< "$games_today" 
+}
 
 games_today=$(get_games_today)
+
+game_id=0 # used in check_if_playing_today 
+check_if_playing_today "$games_today"
+echo "$game_id"
+
+
+
 
 
 #json_data=$(curl -X GET $scoreboard_endpt)
