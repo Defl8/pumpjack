6c0353775260a390f02354ffb884ae6cc417a42b
Wyatt James <wyattsamueljames@gmail.com>
2025-05-20 21:13:38 -0600

    feat: add formatted output for games today

--- pumpjack.sh
+++ pumpjack.sh
@@ -42,10 +42,11 @@ check_if_playing_today(){
 
 check_if_game_live(){
     local game_id="$1"
-    readonly boxscore_endpt="https://api-web.nhle.com/v1/gamecenter/${game_id}/boxscore"
+    local boxscore_endpt="https://api-web.nhle.com/v1/gamecenter/${game_id}/boxscore"
     local game_info=$(curl -sX GET $boxscore_endpt)
     local game_state=$(echo "$game_info" | jq -r ".gameState")
-    if [[ "$game_state" == "LIVE" ]]; then
+    # "FUT" means future
+    if [[ "$game_state" != "OFF" || "$game_state" != "FUT" ]]; then
         return 0
     fi
     return 1
@@ -53,11 +54,12 @@ check_if_game_live(){
 
 format_live(){
     local game_id="$1"
-    readonly boxscore_endpt="https://api-web.nhle.com/v1/gamecenter/${game_id}/boxscore"
+    local boxscore_endpt="https://api-web.nhle.com/v1/gamecenter/${game_id}/boxscore"
     local game_info=$(curl -sX GET $boxscore_endpt)
+    local game_state=$(echo "$game_info" | jq -r ".gameState")
+
     local away_team=$(echo "$game_info" | jq -r '.awayTeam.abbrev')
     local home_team=$(echo "$game_info" | jq -r '.homeTeam.abbrev')
-
     local away_score=$(echo "$game_info" | jq -r '.awayTeam.score')
     local home_score=$(echo "$game_info" | jq -r '.homeTeam.score')
 
@@ -68,27 +70,48 @@ format_live(){
     local period="${period}${period_num}"
     if (( period_num == 3)); then
         period="${period}rd"
+    elif (( period_num == 4)); then
+        period="OT"
     fi
 
-    echo "$away_team $away_score - $period $time_remaining - $home_score $home_team"
+    if [[ "$game_state" != "FINAL" ]]; then
+        echo "$away_team $away_score - $period $time_remaining - $home_score $home_team"
+    else
+        if [[ "$period" != "OT" ]]; then
+            echo "$away_team $away_score - FINAL - $home_score $home_team"
+        else
+            echo "$away_team $away_score - FINAL/OT - $home_score $home_team"
+        fi
+    fi
+}
+
+format_later_today(){
+    local game_id="$1"
+    local boxscore_endpt="https://api-web.nhle.com/v1/gamecenter/${game_id}/boxscore"
+    local game_info=$(curl -sX GET $boxscore_endpt)
+
+    local away_team=$(echo "$game_info" | jq -r '.awayTeam.abbrev')
+    local home_team=$(echo "$game_info" | jq -r '.homeTeam.abbrev')
+    local game_time_utc=$(echo "$game_info" | jq -r '.startTimeUTC')
+    local game_time_mst=$(date -d "$game_time_utc" +"%a @ %H:%M")
+    echo "$game_time_mst | $away_team @ $home_team"
+}
+
+get_next_game(){
+   local schedule_week_endpt="https://api-web.nhle.com/v1/club-schedule/${edm_abbrev}/week/${current_date}" 
+
 }
 
 games_today=$(get_games_today)
-
 game_id=0 # used in check_if_playing_today 
+
 if check_if_playing_today "$games_today"; then
-    # TODO: Remove later
     if check_if_game_live "$game_id"; then
-    #if check_if_game_live 2024030311; then
-        echo "The game is live"
+        live_output=$(format_live "$game_id")
+        echo "$live_output"
     else
-        echo "Game is not live"
+        later_today_output=$(format_later_today "$game_id")
+        echo "$later_today_output"
     fi
 else
-    # TODO: Remove later, whole if block
-    if check_if_game_live 2024030311; then
-        echo "The game is live"
-    else
-        echo "Game is not live"
-    fi
 fi
